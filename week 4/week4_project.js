7052581a0449a92068eea9f66530fc4fa112c0be
melissarvogel <63073544+melissarvogel@users.noreply.github.com>
2020-06-30 12:41:06 -0400

    Add files via upload

---
+++ week 4/week4_project.js
@@ -0,0 +1,6 @@
+
+const firstbutton = document.getElementById("button1").addEventListener('click', function(){
+    if(firstbutton.addEventListener(click === true))
+        axios.get(swapi.dev/api/people/2)
+}
+)
